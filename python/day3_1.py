e170ef1356a162335956494af1b6660f761adab7
Ula Kaminska <ursz.kam@gmail.com>
2022-12-03 09:37:23 +0100

    initial commit

---
+++ python/day3_1.py
@@ -0,0 +1,23 @@
+from string import ascii_uppercase, ascii_lowercase
+
+
+def _get_input():
+    with open('./input/day3.txt', 'r') as data:
+        return (cal for cal in data.readlines())
+    
+    
+def get_priorities():
+    components = _get_input()
+    total = 0
+    chars = ascii_lowercase + ascii_uppercase
+    
+    for component in components:
+        component = component.strip('\n')
+        mid = len(component)//2
+        
+        item = set(component[:mid]).intersection(set(component[mid:]))
+        item = item.pop()
+        
+        total += chars.index(item)+1
+        
+    return total
---
+++ python/day3_2.py
@@ -0,0 +1,21 @@
+import re
+from string import ascii_uppercase, ascii_lowercase
+
+
+def _get_input():
+    with open('./input/day3.txt', 'r') as data:
+        return  data.readlines()
+    
+    
+def get_priorities():
+    components = _get_input()
+    total = 0
+    chars = ascii_lowercase + ascii_uppercase
+    
+    for i in range(0,len(components),3):
+        item = set(components[i]).intersection(set(components[i+1]).intersection(components[i+2]))
+        item = re.sub('\n', '' , ''.join(item))
+        
+        total += chars.index(item) + 1
+        
+    return total
