2ee94ddd353381d400fa3feff6c41a124f76debb
Ula Kaminska <ursz.kam@gmail.com>
2022-12-09 02:42:04 +0100

    refactor: change static to classmethod

--- python/day5/classes.py
+++ python/day5/classes.py
@@ -1,60 +1,39 @@
-class Reader():
-    def __init__(self, dir):
-        self.dir = dir
+class Reader:
+    def __init__(self, directory):
+        self.directory = directory
         self.data = []
-        
-    
+
     def read(self):
-        with open(self.dir, 'r') as data:
+        with open(self.directory, "r") as data:
             self.data = data.readlines()
-        
-        
-    def __iter__(self):
-        self.line = 0 
-        return self
-         
-            
-    def __next__(self):
-        if self.line < len(self.data):
-            result = self.data[self.line]
-            self.line += 1
-            return result
-        else:
-            raise StopIteration
-                
+
 
 class Stack:
     stack_counter = 0
     stack_dict = {}
-    
-    
+
     def __init__(self, name):
         self.name = name
-        self.crates = ''
+        self.crates = ""
         Stack.stack_counter += 1
         Stack.stack_dict[Stack.stack_counter] = self
-        
-        
-    def push(self, crates, reversed=False):
-        self.crates += crates[::-1] if reversed else crates 
-      
-        
+
+    def push(self, crates, reversed):
+        self.crates += crates[::-1] if reversed else crates
+
     def pop(self, num):
-        pop = self.crates[-num:] 
+        pop = self.crates[-num:]
         self.crates = self.crates[:-num]
         return pop
-    
-    
-    @staticmethod
+
+    @classmethod
     def move(cls, stack1, stack2, num, reversed):
-        crates = cls.stack_dist[stack1].pop(num)
+        crates = cls.stack_dict[stack1].pop(num)
         cls.stack_dict[stack2].push(crates, reversed)
-    
-    
-    @staticmethod
+
+    @classmethod
     def peek_all(cls):
-        tops = ''
+        tops = ""
         for stack in cls.stack_dict.values():
             tops += stack.crates[-1]
         print(tops)
-    
