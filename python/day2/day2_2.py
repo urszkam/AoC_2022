d122e681dda65f63003db5f836070cf13588be60
Ula Kaminska <ursz.kam@gmail.com>
2022-12-09 02:35:31 +0100

    style: change scores dict

--- python/day2/day2_2.py
+++ python/day2/day2_2.py
@@ -5,9 +5,17 @@ def count_score():
     rounds = _get_input()
     total = 0
     win = 6; draw = 3; rock = 1; paper = 2; scissors = 3
-    scores = {'A X': scissors, 'A Y': draw+rock, 'A Z': win+paper,
-              'B X': rock, 'B Y': draw+paper, 'B Z': win+scissors,
-              'C X': paper, 'C Y': draw+scissors, 'C Z': win+rock}
+    scores = {
+        "A X": scissors,
+        "A Y": draw + rock,
+        "A Z": win + paper,
+        "B X": rock,
+        "B Y": draw + paper,
+        "B Z": win + scissors,
+        "C X": paper,
+        "C Y": draw + scissors,
+        "C Z": win + rock,
+    }
     for round in rounds:
         total += scores[round[:-1]]
         
