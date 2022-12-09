dce27300f2c81aa9cdf9b0c5c9e653210c03af65
Ula Kaminska <ursz.kam@gmail.com>
2022-12-09 02:59:17 +0100

    fix: change size counting

--- python/day7/day7_1_oop.py
+++ python/day7/day7_1_oop.py
@@ -21,11 +21,13 @@ def sum_file_sizes():
                 dir_obj = Directory(full_dir)
                 dirs.update({dir_obj.name: dir_obj})
                 Directory.go_down(dir_obj)
-                
-        elif re.match('\d+\s[\w.]*', command):
-            size, file = re.findall('(\d+)\s([\w.]+)', command.strip())[0]
-            Directory.add_file(file, size)
-        
+
+        elif re.match("\d+", command):
+            search_result = re.search("\d+", command.strip())
+            size = search_result.group()
+
+            Directory.add_file(size)
+
     sizes = {}
     for k,directory in dirs.items():
         dir_size = directory.count_dir_size()
