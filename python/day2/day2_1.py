55b2e25337e3cdf38be62973202bf15edd0cbae8
Ula Kaminska <ursz.kam@gmail.com>
2022-12-09 02:34:07 +0100

    style: change scores dict

--- python/day2/day2_1.py
+++ python/day2/day2_1.py
@@ -6,9 +6,17 @@ def _get_input():
 def count_score():
     rounds = _get_input()
     total = 0
-    scores = {'A X': 4, 'A Y': 8, 'A Z': 3,
-              'B X': 1, 'B Y': 5, 'B Z': 9,
-              'C X': 7, 'C Y': 2, 'C Z': 6}
+    scores = {
+        "A X": 4,
+        "A Y": 8,
+        "A Z": 3,
+        "B X": 1,
+        "B Y": 5,
+        "B Z": 9,
+        "C X": 7,
+        "C Y": 2,
+        "C Z": 6,
+    }
     for round in rounds:
         total += scores[round[:-1]]
         
